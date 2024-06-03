188e9e96dbba61a48c312e2b872273945a240c71
levgorbunov1 <lev.gorbunov@digital.justice.gov.uk>
2024-06-03 17:19:24 +0100

    rename form keys in github service

--- app/main/services/github_service.py
+++ app/main/services/github_service.py
@@ -13,16 +13,16 @@ class GithubService:
         self.repo = self.github_client_core_api.get_repo(repository)
 
     def submit_issue(self, form_data: dict) -> None:
-        title = f"[DNS] {form_data['new_domain_name']}"
+        title = f"[DNS] {form_data['domain_name']}"
         body = (
             f"**Requestor Name:** {form_data['requestor_name']}\n\n"
-            f"**MoJ Service Owner:** {form_data['moj_service_owner']}\n\n"
-            f"**Service Area Name:** {form_data['service_area_name']}\n\n"
-            f"**Business Area Name:** {form_data['business_area_name']}\n\n"
-            f"**New Domain Name:** {form_data['new_domain_name']}\n\n"
-            f"**New Service Description:** {form_data['new_service_description']}\n\n"
-            f"**Purpose of New Domain:** {form_data['new_domain_purpose']}\n\n"
-            f"**DNS Record Type:** {form_data['type_of_record']}\n\n"
+            f"**MoJ Service Owner:** {form_data['service_owner']}\n\n"
+            f"**Service Area Name:** {form_data['service_area']}\n\n"
+            f"**Business Area Name:** {form_data['business_area']}\n\n"
+            f"**New Domain Name:** {form_data['domain_name']}\n\n"
+            f"**New Service Description:** {form_data['service_description']}\n\n"
+            f"**Purpose of New Domain:** {form_data['domain_purpose']}\n\n"
+            f"**DNS Record Type:** {form_data['record_type']}\n\n"
             f"**NS Details:** {form_data.get('ns_details', 'N/A')}\n\n"
             f"Template request for adding a new justice.gov.uk subdomain."
         )
