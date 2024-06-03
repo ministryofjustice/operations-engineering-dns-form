7ea45868f741b456f67dc68107b6e5de59b0c18b
levgorbunov1 <lev.gorbunov@digital.justice.gov.uk>
2024-06-03 16:39:54 +0100

    error handling

--- app/main/routes/make_request.py
+++ app/main/routes/make_request.py
@@ -2,12 +2,16 @@ from flask import (
     Blueprint,
     request,
     current_app,
-    render_template
+    render_template,
+    flash
 )
 
 make_request_route = Blueprint("make_request_route", __name__)
 
 @make_request_route.route("/submit-dns-request", methods = ["POST"])
 def submit_dns_request():
-    current_app.github_service.submit_issue(request.form.to_dict())
-    return render_template("pages/request-complete.html")
+    try:
+        current_app.github_service.submit_issue(request.form.to_dict())
+        return render_template("pages/request-complete.html")
+    except:
+        flash("Failed to submit request")
