0e339537923dffe7c54cdd4bb3bde1dbbc47b927
levgorbunov1 <lev.gorbunov@digital.justice.gov.uk>
2024-06-04 10:49:15 +0100

    remove patch

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -12,13 +12,12 @@ class TestSubmitDNSRequest(unittest.TestCase):
         self.app.config["SECRET_KEY"] = "test_flask"
         self.client = self.app.test_client()
  
-    @patch.object(GithubService, "submit_issue")
-    def test_submit_dns_request(self, mock_submit_issue):
+    def test_submit_dns_request(self):
         form_data = {'requestor_name': '', 'service_owner': '', 'service_area': '', 'business_area': 'hmpps', 'domain_name': '', 'service_description': '', 'domain_purpose': '', 'record_type': 'ns', 'ns_details': ''}
-        response = self.app.test_client().post("/submit-dns-request", data=form_data)
+        response = self.client.post("/submit-dns-request", data=form_data)
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.request.path, "/submit-dns-request")
-        mock_submit_issue.assert_called_once_with(form_data)
+        self.github_service.submit_issue.assert_called_once_with(form_data)
 
     
