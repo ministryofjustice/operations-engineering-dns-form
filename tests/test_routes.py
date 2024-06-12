ee98f184131ca2a4eb6de9faa6813ac1feb5a51f
levgorbunov1 <lev.gorbunov@digital.justice.gov.uk>
2024-06-12 15:27:22 +0100

    uncomment test

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -12,10 +12,10 @@ class TestSubmitDNSRequest(unittest.TestCase):
         self.app.config["SECRET_KEY"] = "test_flask"
         self.client = self.app.test_client()
 
-    # def test_submit_dns_request(self):
-    #     form_data = {'requestor_name': 'g', 'requestor_email': 'g.g@gmail.com', 'service_owner': 'f', 'service_area': 'f', 'business_area': 'hmpps', 'domain_name': 'f', 'service_description': 'f', 'domain_purpose': 'f', 'record_type': 'ns', 'ns_details': ''}
-    #     response = self.client.post("/submit-dns-request", data=form_data)
+    def test_submit_dns_request(self):
+        form_data = {'requestor_name': 'g', 'requestor_email': 'g.g@gmail.com', 'service_owner': 'f', 'service_area': 'f', 'business_area': 'hmpps', 'domain_name': 'f', 'service_description': 'f', 'domain_purpose': 'f', 'record_type': 'ns', 'ns_details': ''}
+        response = self.client.post("/submit-dns-request", data=form_data)
 
-    #     self.assertEqual(response.status_code, 200)
-    #     self.assertEqual(response.request.path, "/submit-dns-request")
-    #     self.github_service.submit_issue.assert_called_once_with(form_data)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.request.path, "/submit-dns-request")
+        self.github_service.submit_issue.assert_called_once_with(form_data)
