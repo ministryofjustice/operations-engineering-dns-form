5d5b79185f761c2b4476df4c17f7b2598a8cd67a
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2024-07-12 16:45:53 +0100

    :sparkles: Enable GitHub issue and PR creation from form data
    This commit introduces a feature where form data from the `create-record` route is now used to submit an issue and create a new pull request on GitHub. On successful submission, users are redirected to a confirmation page with the URL of the created PR, enhancing feedback and transparency for users.

--- app/main/routes/main.py
+++ app/main/routes/main.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, redirect, render_template, request, url_for
+from flask import Blueprint, current_app, redirect, render_template, request, url_for
 
 main = Blueprint("main", __name__)
 
@@ -28,7 +28,7 @@ def new_hosted_zone():
     return render_template("pages/new_hosted_zone_form.html")
 
 
-@ main.route("/change-record", methods=["GET", "POST"])
+@main.route("/change-record", methods=["GET", "POST"])
 def change_record():
     if request.method == "POST":
         form_data = request.form.to_dict()
@@ -36,9 +36,14 @@ def change_record():
     return render_template("pages/change_record_form.html")
 
 
-@ main.route("/create-record", methods=["GET", "POST"])
+@main.route("/create-record", methods=["GET", "POST"])
 def create_record():
     if request.method == "POST":
         form_data = request.form.to_dict()
-        return redirect("/")
+
+        issue = current_app.github_service.submit_issue(form_data)
+        pr_link = current_app.github_service.create_pr(form_data, issue)
+
+        return render_template("pages/confirmation.html", pr_url=pr_link)
+
     return render_template("pages/create_record_form.html")
