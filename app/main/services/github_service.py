373a6ecfd0f196b51c2f73cf2a7507238e82cf73
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2024-07-12 16:45:09 +0100

    :sparkles: Extend GithubService for PRs and improved issue creation
    This commit greatly extends the capabilities of GithubService. A new function 'create_pr' has been added to automate the process of creating a Pull Request on GitHub based on a change in DNS record. It handles all types of records: NS, A, MX, CNAME, TXT, and alias. It creates/updates the YAML file representing the DNS configuration, commits it to a new branch, and finally creates the PR.
    
    In addition, the 'submit_issue' function has been enhanced to provide comprehensive details about the requested DNS change, depending on the type of DNS record involved. An exception handling mechanism is also implemented when trying to create an issue on GitHub.
    
    Finally, the constructor now separately handles the repositories for issues and pull requests, providing more flexibility to work with different repositories.

--- app/main/services/github_service.py
+++ app/main/services/github_service.py
@@ -1,37 +1,141 @@
 import logging
 
+import yaml
 from github import Github
+from github.GithubException import GithubException
 
 logger = logging.getLogger(__name__)
 
 
 # pylint: disable=R0903
 class GithubService:
-    def __init__(self, org_token: str, repository: str) -> None:
-        self.repository = repository
+    def __init__(
+        self, org_token: str, issues_repository: str, pull_request_repository: str
+    ) -> None:
+        self.issues_repository = issues_repository
+        self.pull_request_repository = pull_request_repository
         self.github_client_core_api: Github = Github(org_token)
-        self.repo = self.github_client_core_api.get_repo(repository)
+        self.issues_repo = self.github_client_core_api.get_repo(issues_repository)
+        self.pr_repo = self.github_client_core_api.get_repo(pull_request_repository)
 
     def submit_issue(self, form_data: dict) -> int:
-        title = f"[DNS] {form_data['domain_name']}"
+        title = f"[DNS] Add record for {form_data['domain_name']}"
         body = (
             f"**Requestor Name:** {form_data['requestor_name']}\n\n"
             f"**Requestor Email:** {form_data['requestor_email']}\n\n"
             f"**MoJ Service Owner:** {form_data['service_owner']}\n\n"
             f"**Service Area Name:** {form_data['service_area']}\n\n"
-            f"**Business Area Name:** {form_data['business_area']}\n\n"
-            f"**New Domain Name:** {form_data['domain_name']}\n\n"
-            f"**New Service Description:** {form_data['service_description']}\n\n"
-            f"**Purpose of New Domain:** {form_data['domain_purpose']}\n\n"
+            f"**Domain Name:** {form_data['domain_name']}\n\n"
+            f"**TTL:** {form_data['ttl']}\n\n"
+            f"**Record Name:** {form_data['record_name']}\n\n"
             f"**DNS Record Type:** {form_data['record_type']}\n\n"
-            f"**NS Details:** {form_data.get('ns_details', 'N/A')}\n\n"
-            f"Template request for adding a new justice.gov.uk subdomain."
         )
-        if form_data["record_type"] == "cname" and "cname_value" in form_data:
+
+        if form_data["record_type"] == "ns":
+            body += f"**NS Values:** {form_data['ns_values']}\n\n"
+        elif form_data["record_type"] == "a":
+            body += f"**A Record Value:** {form_data['a_value']}\n\n"
+        elif form_data["record_type"] == "mx":
+            body += f"**MX Values:** {form_data['mx_values']}\n\n"
+        elif form_data["record_type"] == "cname":
             body += f"**CNAME Value:** {form_data['cname_value']}\n\n"
+        elif form_data["record_type"] == "txt":
+            body += f"**TXT Value:** {form_data['txt_value']}\n\n"
+        elif form_data["record_type"] == "alias":
+            body += (
+                f"**Evaluate Target Health:** {form_data['evaluate_target_health']}\n\n"
+                f"**Hosted Zone ID:** {form_data['hosted_zone_id']}\n\n"
+                f"**Alias Name:** {form_data['alias_name']}\n\n"
+                f"**Alias Type:** {form_data['alias_type']}\n\n"
+            )
 
-        labels = ["dns-request", "add-subdomain-request"]
+        body += "Template request for adding a new DNS record."
 
-        issue = self.repo.create_issue(title=title, body=body, labels=labels)
-        logger.info("Issue created: %s", title)
-        return issue.number
+        labels = ["dns-request", "add-record-request"]
+
+        try:
+            issue = self.issues_repo.create_issue(title=title, body=body, labels=labels)
+            logger.info("Issue created: %s", title)
+            return issue.number
+        except GithubException as e:
+            logger.error("Error creating issue: %s", e)
+            raise
+
+    def create_pr(self, form_data: dict, issue_number) -> str:
+        branch_name = (
+            f"add-{form_data['domain_name']}-record-{form_data['record_name']}"
+        )
+        issue_number = (
+            f"https://github.com/{self.issues_repository}/issues/{issue_number}"
+        )
+        file_path = f"hostedzones/{form_data['domain_name']}.yaml"
+
+        record_name = form_data["record_name"]
+        record_data = {
+            "ttl": form_data["ttl"],
+            "type": (form_data["record_type"].upper()),
+        }
+
+        if form_data["record_type"] == "ns":
+            record_data["values"] = form_data["ns_values"].split(",")
+        elif form_data["record_type"] == "a":
+            record_data["value"] = form_data["a_value"]
+        elif form_data["record_type"] == "mx":
+            record_data["values"] = form_data["mx_values"].split("\n")
+        elif form_data["record_type"] == "cname":
+            record_data["value"] = form_data["cname_value"]
+        elif form_data["record_type"] == "txt":
+            record_data["value"] = form_data["txt_value"]
+        elif form_data["record_type"] == "alias":
+            record_data["value"] = {
+                "evaluate-target-health": form_data["evaluate_target_health"].lower()
+                == "true",
+                "hosted-zone-id": form_data["hosted_zone_id"],
+                "name": form_data["alias_name"],
+                "type": form_data["alias_type"],
+            }
+
+        main_ref = self.pr_repo.get_git_ref("heads/main")
+        main_sha = main_ref.object.sha
+        self.pr_repo.create_git_ref(ref=f"refs/heads/{branch_name}", sha=main_sha)
+
+        try:
+            existing_content = self.pr_repo.get_contents(file_path, ref="main")
+            file_content = existing_content.decoded_content.decode("utf-8")
+            yaml_content = yaml.safe_load(file_content)
+        except GithubException:
+            yaml_content = {}
+
+        yaml_content[record_name] = record_data
+
+        sorted_yaml_content = dict(sorted(yaml_content.items()))
+
+        new_file_content = yaml.dump(sorted_yaml_content, default_flow_style=False)
+
+        commit_message = (
+            f"✨ Add {form_data['record_type']} record to {form_data['domain_name']}"
+        )
+        if "existing_content" in locals():
+            self.pr_repo.update_file(
+                path=file_path,
+                message=commit_message,
+                content=new_file_content,
+                sha=existing_content.sha,
+                branch=branch_name,
+            )
+        else:
+            self.pr_repo.create_file(
+                path=file_path,
+                message=commit_message,
+                content=new_file_content,
+                branch=branch_name,
+            )
+
+        pr = self.pr_repo.create_pull(
+            title=f"✨ Add {form_data['record_type']} record to {form_data['domain_name']}",
+            body=f"Resolves {issue_number}",
+            head=branch_name,
+            base="main",
+        )
+        logger.info("Pull request created: %s", pr.html_url)
+        return pr.html_url
