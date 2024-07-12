51864467d22d7f9c160f6b78547672bff66a6e0b
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2024-07-12 15:02:57 +0100

    :sparkles: Implement DNS Change Request routing
    This commit introduces new routes to handle different types of DNS change requests. The main page now renders "main_form.html" which contains a form to select the type of DNS change required: "new_hostedzone", "change_record", or "create_record". Based on this selection, it redirects to the corresponding route. These new routes ("new-hostedzone", "change-record", "create-record") handle both GET and POST methods; in case of a POST request, they process the form data then redirect back to the home page.

--- app/main/routes/main.py
+++ app/main/routes/main.py
@@ -1,11 +1,44 @@
-from flask import (
-    Blueprint,
-    render_template
-)
+from flask import Blueprint, redirect, render_template, request, url_for
 
 main = Blueprint("main", __name__)
 
 
 @main.route("/")
 def index():
-    return render_template("pages/home.html")
+    return render_template("pages/main_form.html")
+
+
+@main.route("/select-change-type", methods=["POST"])
+def select_change_type():
+    change_type = request.form["change_type"]
+    if change_type == "new_hostedzone":
+        return redirect("/new-hostedzone")
+    elif change_type == "change_record":
+        return redirect("/change-record")
+    elif change_type == "create_record":
+        return redirect("/create-record")
+    return redirect(url_for("main.index"))
+
+
+@main.route("/new-hostedzone", methods=["GET", "POST"])
+def new_hosted_zone():
+    if request.method == "POST":
+        form_data = request.form.to_dict()
+        return redirect("/")
+    return render_template("pages/new_hosted_zone_form.html")
+
+
+@ main.route("/change-record", methods=["GET", "POST"])
+def change_record():
+    if request.method == "POST":
+        form_data = request.form.to_dict()
+        return redirect("/")
+    return render_template("pages/change_record_form.html")
+
+
+@ main.route("/create-record", methods=["GET", "POST"])
+def create_record():
+    if request.method == "POST":
+        form_data = request.form.to_dict()
+        return redirect("/")
+    return render_template("pages/create_record_form.html")
