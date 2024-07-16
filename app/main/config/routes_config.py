04cba9bd92c544627d742f0fd4437219cdb13585
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2024-07-16 12:02:23 +0100

    :fire: Remove unused routes
    The routes previously configured have been replaced following a restructure.

--- app/main/config/routes_config.py
+++ app/main/config/routes_config.py
@@ -1,11 +1,8 @@
-from flask import Flask
-
 from app.main.routes.main import main
 from app.main.routes.robots import robot_route
-from app.main.routes.make_request import make_request_route
+from flask import Flask
 
 
 def configure_routes(app: Flask) -> None:
     app.register_blueprint(main)
     app.register_blueprint(robot_route)
-    app.register_blueprint(make_request_route)
