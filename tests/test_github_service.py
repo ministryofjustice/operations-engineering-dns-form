f01920d2facdf6f7fc8dd829530f4df21ab53e1d
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2024-07-16 10:29:28 +0100

    :white_check_mark: Add a test to validate the accuracy of the submit_issue method

--- tests/test_github_service.py
+++ tests/test_github_service.py
@@ -1,50 +1,38 @@
 import unittest
-from unittest.mock import patch, MagicMock
+from unittest.mock import MagicMock, patch
+
 from app.main.services.github_service import GithubService
 
 
-@patch("github.Github.__new__")
 class TestGithubService(unittest.TestCase):
-
-    def test_submit_issue(self, mock_github):
-        mock_repo = MagicMock()
+    @patch("app.main.services.github_service.Github")
+    def test_submitting_an_issue(self, mock_github):
+        mock_repository = MagicMock()
         mock_github_instance = mock_github.return_value
-        mock_github_instance.get_repo.return_value = mock_repo
+        mock_github_instance.get_repo.return_value = mock_repository
 
+        mock_issue = MagicMock()
+        mock_issue.number = 134
+        mock_repository.create_issue.return_value = mock_issue
+
+        github_service = GithubService("token", "issues_repo", "pr_repo")
         form_data = {
-            'requestor_name': 'John Requestor',
-            'requestor_email': 'john.requestor@digital.justice.gov.uk',
-            'service_owner': 'Jane Owner',
-            'service_area': 'IT Department',
-            'business_area': 'HMPPS',
-            'domain_name': 'example.justice.gov.uk',
-            'service_description': 'A new service description.',
-            'domain_purpose': 'Primary purpose of the new domain.',
-            'record_type': 'A',
-            'ns_details': 'Justification for NS delegation.'
+            "requestor_name": "test",
+            "requestor_email": "test@test.com",
+            "service_owner": "test",
+            "service_area": "test",
+            "domain_name": "test.com",
+            "ttl": "300",
+            "record_name": "test",
+            "record_type": "a",
+            "a_value": "192.0.2.1",
         }
 
-        github_service = GithubService('dummy_token', 'dummy_user/dummy_repo')
+        issue = github_service.submit_issue(form_data)
 
-        github_service.submit_issue(form_data)
-
-        mock_repo.create_issue.assert_called_once()
-        # pylint: disable=W0612
-        args, kwargs = mock_repo.create_issue.call_args
-        self.assertIn('[DNS] example.justice.gov.uk', kwargs['title'])
-        self.assertIn('**Requestor Name:** John Requestor', kwargs['body'])
-        self.assertIn('**Requestor Email:** john.requestor@digital.justice.gov.uk', kwargs['body'])
-        self.assertIn('**MoJ Service Owner:** Jane Owner', kwargs['body'])
-        self.assertIn('**Service Area Name:** IT Department', kwargs['body'])
-        self.assertIn('**Business Area Name:** HMPPS', kwargs['body'])
-        self.assertIn('**New Domain Name:** example.justice.gov.uk', kwargs['body'])
-        self.assertIn('**New Service Description:** A new service description.', kwargs['body'])
-        self.assertIn('**Purpose of New Domain:** Primary purpose of the new domain.', kwargs['body'])
-        self.assertIn('**DNS Record Type:** A', kwargs['body'])
-        self.assertIn('**NS Details:** Justification for NS delegation.', kwargs['body'])
-        self.assertIn('Template request for adding a new justice.gov.uk subdomain.', kwargs['body'])
-        self.assertEqual(kwargs['labels'], ["dns-request", "add-subdomain-request"])
+        mock_repository.create_issue.assert_called_once()
+        self.assertEqual(issue, 134)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
