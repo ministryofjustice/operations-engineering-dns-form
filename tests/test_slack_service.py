e181e1e5c020689435bc3828bb56a3b99653d0c4
abachleda-baca <agnieszka.bachleda@digital.justice.gov.uk>
2024-09-05 14:20:23 +0100

    modify test cases

--- tests/test_slack_service.py
+++ tests/test_slack_service.py
@@ -16,32 +16,39 @@ class TestSendMessageToPlainTextChannelName(unittest.TestCase):
     def setUp(self):
         self.channel = {'name': self.CHANNEL_NAME, 'id': self.CHANNEL_ID}
         self.slack_client = MagicMock()
-        self.slack_service = SlackService("")
+        self.slack_service = SlackService("test_slack_token")
         self.slack_client.conversations_list.return_value = {
             'channels': [self.channel], 'response_metadata': self.RESPONSE_METADATA}
         self.slack_service.slack_client = self.slack_client
 
-    def test_send_message_to_plaintext_channel_name(self):
+    @patch("logging.info")
+    def test_send_message_to_plaintext_channel_name(self, mock_logging_info):
         self.slack_client.chat_postMessage.return_value = self.RESPONSE_OK
         self.slack_service.send_message_to_plaintext_channel_name(
             self.MESSAGE, self.CHANNEL_NAME)
         self.slack_client.chat_postMessage.assert_called_once_with(
             channel=self.CHANNEL_ID, text=self.MESSAGE)
+        mock_logging_info.assert_called_once_with("Message sent to channel %s", self.CHANNEL_NAME)
 
-    def test_send_message_to_plaintext_channel_name_when_no_channel_name_exists(self):
+    @patch("logging.error")
+    def test_send_message_to_plaintext_channel_name_when_no_channel_name_exists(self, mock_logging_error):
         response = {'channels': [],
                     'response_metadata': self.RESPONSE_METADATA}
         self.slack_client.conversations_list.return_value = response
         self.slack_service.send_message_to_plaintext_channel_name(
             self.MESSAGE, self.CHANNEL_NAME)
         self.slack_client.chat_postMessage.assert_not_called()
+        mock_logging_error.assert_called_once_with("Could not find channel %s", self.CHANNEL_NAME)
 
-    def test_send_message_to_plaintext_channel_name_when_response_not_okay(self):
+    @patch("logging.error")
+    def test_send_message_to_plaintext_channel_name_when_response_not_okay(self, mock_logging_error):
         self.slack_client.chat_postMessage.return_value = self.RESPONSE_ERROR
         self.slack_service.send_message_to_plaintext_channel_name(
             self.MESSAGE, self.CHANNEL_NAME)
         self.slack_client.chat_postMessage.assert_called_once_with(
             channel=self.CHANNEL_ID, text=self.MESSAGE)
+        mock_logging_error.assert_called_once_with("Error sending message to channel %s: %s",
+                                                   self.CHANNEL_NAME, self.RESPONSE_ERROR['error'])
 
     def test_lookup_channel_id(self):
         result = self.slack_service.lookup_channel_id(self.CHANNEL_NAME)
